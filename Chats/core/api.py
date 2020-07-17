3aa0cd10d32962812a695644be16999946111fa5
krprabhatSingh <prabhatk287@gmail.com>
2020-07-17 17:06:07 +0530

    Add files via upload

---
+++ Chats/core/api.py
@@ -0,0 +1,90 @@
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication
+
+from chat import settings
+from core.serializers import MessageModelSerializer, UserModelSerializer
+from core.models import MessageModel
+
+#For showing only online users by using session
+from django.contrib.sessions.models import Session
+from django.utils import timezone
+
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+    """
+    SessionAuthentication scheme used by DRF. DRF's SessionAuthentication uses
+    Django's session framework for authentication which requires CSRF to be
+    checked. In this case we are going to disable CSRF tokens for the API.
+    """
+
+    def enforce_csrf(self, request):
+        return
+
+
+class MessagePagination(PageNumberPagination):
+    """
+    Limit message prefetch to one page.
+    """
+    page_size = settings.MESSAGES_TO_LOAD
+
+
+class MessageModelViewSet(ModelViewSet):
+    queryset = MessageModel.objects.all()
+    serializer_class = MessageModelSerializer
+    allowed_methods = ('GET', 'POST', 'HEAD', 'OPTIONS')
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+    pagination_class = MessagePagination
+
+    def list(self, request, *args, **kwargs):
+        self.queryset = self.queryset.filter(Q(recipient=request.user) |
+                                             Q(user=request.user))
+        target = self.request.query_params.get('target', None)
+        if target is not None:
+            self.queryset = self.queryset.filter(
+                Q(recipient=request.user, user__username=target) |
+                Q(recipient__username=target, user=request.user))
+        return super(MessageModelViewSet, self).list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        msg = get_object_or_404(
+            self.queryset.filter(Q(recipient=request.user) |
+                                 Q(user=request.user),
+                                 Q(pk=kwargs['pk'])))
+        serializer = self.get_serializer(msg)
+        return Response(serializer.data)
+
+
+class UserModelViewSet(ModelViewSet):
+
+
+
+    serializer_class = UserModelSerializer
+    allowed_methods = ('GET', 'HEAD', 'OPTIONS')
+    pagination_class = None
+    # Get all user
+
+    def list(self, request, *args, **kwargs):
+        # Query all non-expired sessions
+        # use timezone.now() instead of datetime.now() in latest versions of Django
+        sessions = Session.objects.filter(expire_date__gte=timezone.now())
+        uid_list = []
+
+        # Build a list of user ids from that query
+        for session in sessions:
+            data = session.get_decoded()
+            uid_list.append(data.get('_auth_user_id', None))
+
+        # Query all logged in users based on id list
+        print(User.objects.filter(id__in=uid_list))
+
+        # It will show the list of users
+        queryset = User.objects.filter(id__in=uid_list)
+        # Get all users except yourself
+        #print("Hit Refresh for User")
+        self.queryset = queryset.exclude(id=request.user.id)
+        return super(UserModelViewSet, self).list(request, *args, **kwargs)
